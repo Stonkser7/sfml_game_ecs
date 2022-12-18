046d421ec9d1ea385e9aaf3d8d1d3729f11a842e
Stonkser <dorofeevvantey@yandex.ru>
2022-12-18 21:32:59 +0400

    Added first functionality to ComponentArray

--- sfml_game_ecs/ComponentArray.h
+++ sfml_game_ecs/ComponentArray.h
@@ -7,7 +7,7 @@
 #include <assert.h>
 
 #include "Entity.h"
-#include 
+#include "Direction.h"
 
 template <typename T>
 class ComponentArray
@@ -37,5 +37,8 @@ inline T& ComponentArray<T>::getData(EntityID entity)
 template<typename T>
 inline void ComponentArray<T>::createData(EntityID entity)
 {
-	m_components.push_back();
+	assert(m_EntityToIndex.find(entity) == m_EntityToIndex.end() && "void ComponentArray<T>::createData(EntityID entity): entity can't have two same components");
+
+	m_components.push_back(T());
+	m_EntityToIndex[entity] = m_components.size() - 1;
 }
