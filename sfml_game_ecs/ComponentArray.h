456370cc13ad8b5e0198242cd512fb9a208fe1e7
Stonkser <dorofeevvantey@yandex.ru>
2022-12-19 23:06:03 +0400

    Added a bit of functionality to ComponentArray class

--- sfml_game_ecs/ComponentArray.h
+++ sfml_game_ecs/ComponentArray.h
@@ -18,10 +18,14 @@ public:
 
 	void createData(EntityID entity);
 
+	void removeData(EntityID entity);
+
 private:
 	std::vector<T> m_components;
 
 	std::unordered_map<EntityID, size_t> m_EntityToIndex;
+
+	std::unordered_map<size_t, EntityID> m_IndexToEntity;
 };
 
 #endif
@@ -41,4 +45,23 @@ inline void ComponentArray<T>::createData(EntityID entity)
 
 	m_components.push_back(T());
 	m_EntityToIndex[entity] = m_components.size() - 1;
+	m_IndexToEntity[m_components.size() - 1] = entity;
+}
+
+template<typename T>
+inline void ComponentArray<T>::removeData(EntityID entity)
+{
+	assert(m_EntityToIndex.find(entity) != m_EntityToIndex.end() && "void ComponentArray<T>::removeData(EntityID entity): attempt to remove non-existent component");
+
+	size_t last_i = m_components.size() - 1;
+	EntityID last_e = m_IndexToEntity[last_i];
+
+	m_components[m_EntityToIndex[entity]] = m_components[last_i];
+
+	m_EntityToIndex[m_IndexToEntity[last_i]] = m_EntityToIndex[entity];
+	m_IndexToEntity[m_EntityToIndex[entity]] = last_e;
+
+	m_EntityToIndex.erase(entity);
+	m_IndexToEntity.erase(last_i);
+	m_components.pop_back();
 }
